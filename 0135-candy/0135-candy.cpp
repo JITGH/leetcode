d474c1938843b1266c518ab700c8a1142cf038b5
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-08-16 23:14:40 +0530

    Time: 3 ms (99.88%), Space: 19.9 MB (78.07%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,30 +1,86 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int n=ratings.size();
-        vector<int>left(n);
-        vector<int>right(n);
-        left[0]=1;
-        right[n-1]=1;
-        for(int i=1;i<n;i++){
-            if(ratings[i]>ratings[i-1]){
-                left[i]=left[i-1]+1;
+        // TC -> O(3N)
+        // int n=ratings.size();
+        // vector<int>left(n);
+        // vector<int>right(n);
+        // left[0]=1;
+        // right[n-1]=1;
+        // for(int i=1;i<n;i++){
+        //     if(ratings[i]>ratings[i-1]){
+        //         left[i]=left[i-1]+1;
 
-            }else{
-                left[i]=1;
-            }
-        }
+        //     }else{
+        //         left[i]=1;
+        //     }
+        // }
 
-        for(int i=n-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]){
-                right[i]=right[i+1]+1;
-            }else{
-                right[i]=1;
+        // for(int i=n-2;i>=0;i--){
+        //     if(ratings[i]>ratings[i+1]){
+        //         right[i]=right[i+1]+1;
+        //     }else{
+        //         right[i]=1;
+        //     }
+        // }
+        // int sum=0;
+        // for(int i=0;i<n;i++){
+        //     sum+=max(left[i],right[i]);
+        // }
+        // return sum;
+
+
+         int n=ratings.size();
+        //  vector<int>left(n);
+        //  left[0]=1;
+        //  for(int i=1;i<n;i++){
+        //     if(ratings[i]>ratings[i-1]){
+        //         left[i]=left[i-1]+1;
+
+        //     }else{
+        //         left[i]=1;
+        //     }
+        // }
+        // int curr=0;
+        // int right=0;
+        // int sum=max(left[n-1],1);
+        // for(int i=n-2;i>=0;i--){
+        //     if(ratings[i]>ratings[i+1]){
+        //         curr=right+1;
+        //         right=curr;
+        //     }
+        //     else{
+        //         curr=1;
+        //     }
+
+        //     sum+=max(left[i],curr);
+        // }
+        // return sum;
+
+        int sum=1;
+        int i=1;
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                sum+=1;
+                i++;
+               continue;
             }
-        }
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=max(left[i],right[i]);
+            int peak=1;
+            while(i<n && ratings[i]>ratings[i-1]){
+                peak++;
+                sum+=peak;
+                i++;
+            }
+            int down=1;
+            while(i<n && ratings[i]<ratings[i-1]){
+                sum+=down;
+                i++;
+                down++;
+            }
+
+            if(down>peak){
+                sum=sum+(down-peak);
+            }
         }
         return sum;
     }
