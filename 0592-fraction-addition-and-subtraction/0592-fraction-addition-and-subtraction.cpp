0d9cb843401f6c1d24406cb12962b91e853a60e9
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-08-23 11:34:11 +0530

    Time: 3 ms (50.39%), Space: 8.2 MB (11.63%) - LeetHub

--- 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.cpp
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.cpp
@@ -5,46 +5,81 @@ int findgcd(int a,int b){
     return findgcd(b%a,a);
 }
     string fractionAddition(string expression) {
-        int num=0;
-        int denom=1;
-        int i=0;
-        while(i<expression.size()){
-            int curnum=0;
-            int curdenom=0;
+        // int num=0;
+        // int denom=1;
+        // int i=0;
+        // while(i<expression.size()){
+        //     int curnum=0;
+        //     int curdenom=0;
 
-            bool isnegative=false;
+        //     bool isnegative=false;
 
-            if(expression[i]=='-'||expression[i]=='+'){
-                if(expression[i]=='-'){
-                isnegative=true;
-                }
-                i++;
-            }
+        //     if(expression[i]=='-'||expression[i]=='+'){
+        //         if(expression[i]=='-'){
+        //         isnegative=true;
+        //         }
+        //         i++;
+        //     }
 
-            while(isdigit(expression[i])){
-                int val=expression[i]-'0';
-                curnum=curnum*10+val;
-                i++;
-            }
+        //     while(isdigit(expression[i])){
+        //         int val=expression[i]-'0';
+        //         curnum=curnum*10+val;
+        //         i++;
+        //     }
 
-            if(isnegative) curnum*=-1;
+        //     if(isnegative) curnum*=-1;
 
-            i++;
+        //     i++;
 
-            while(i<expression.size() && isdigit(expression[i])){
-                int val=expression[i]-'0';
-                curdenom=curdenom*10+val;
-                i++;
-            }
-            num=num*curdenom+denom*curnum;
-            denom=denom*curdenom;
+        //     while(i<expression.size() && isdigit(expression[i])){
+        //         int val=expression[i]-'0';
+        //         curdenom=curdenom*10+val;
+        //         i++;
+        //     }
+        //     num=num*curdenom+denom*curnum;
+        //     denom=denom*curdenom;
             
+        // }
+        // int gcd=abs(findgcd(num,denom));
+
+        // num/=gcd;
+        // denom/=gcd;
+
+        // return to_string(num)+"/"+to_string(denom);
+
+        int num = 0;
+        int denom = 1;
+
+        // separate expression into signed numbers
+        vector<string> nums;
+        int i = 0;
+        if (expression[0] != '-') expression = '+' + expression;
+        while (i < expression.size()) {
+            int j = i + 1;
+            while (j < expression.size() && expression[j] != '+' &&
+                   expression[j] != '-') {
+                j++;
+            }
+            nums.push_back(expression.substr(i, j - i));
+            i = j;
         }
-        int gcd=abs(findgcd(num,denom));
 
-        num/=gcd;
-        denom/=gcd;
+        for (int i = 0; i < nums.size(); ++i) {
+            size_t pos = nums[i].find('/');
+            int currNum = stoi(nums[i].substr(1, pos - 1));
+            if (nums[i][0] == '-') currNum = -currNum;
+            int currDenom = stoi(nums[i].substr(pos + 1));
 
-        return to_string(num)+"/"+to_string(denom);
+            num = num * currDenom + currNum * denom;
+            denom = denom * currDenom;
+
+            int gcd = abs(findgcd(num, denom));
+
+            num /= gcd;
+            denom /= gcd;
+        }
+
+        return to_string(num) + "/" + to_string(denom);
+
     }
 };
