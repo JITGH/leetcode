3be242b095d1807f4c11d866e822719e442bf25c
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-08-25 11:07:33 +0530

    Time: 5 ms (31.38%), Space: 9.2 MB (59.94%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,28 +1,47 @@
 class Solution {
 public:
-void f(vector<vector<int>>&ans,vector<int>&ds,int freq[],vector<int>&nums){
-    if(ds.size()==nums.size()){
-        ans.push_back(ds);
-        return;
+// void f(vector<vector<int>>&ans,vector<int>&ds,int freq[],vector<int>&nums){
+//     if(ds.size()==nums.size()){
+//         ans.push_back(ds);
+//         return;
+//     }
+
+//     for(int i=0;i<nums.size();i++){
+//         if(!freq[i]){
+//             ds.push_back(nums[i]);
+//             freq[i]=1;
+//             f(ans,ds,freq,nums);
+//             freq[i]=0;
+//             ds.pop_back();
+//         }
+//     }
+// }
+
+    void f(int ind,vector<vector<int>>&ans,vector<int>&nums){
+        if(ind==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+
+        for(int i=ind;i<nums.size();i++){
+            swap(nums[ind],nums[i]);
+            f(ind+1,ans,nums);
+            swap(nums[ind],nums[i]);
+        }
+
+        
     }
 
-    for(int i=0;i<nums.size();i++){
-        if(!freq[i]){
-            ds.push_back(nums[i]);
-            freq[i]=1;
-            f(ans,ds,freq,nums);
-            freq[i]=0;
-            ds.pop_back();
-        }
-    }
-}
+
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
-        vector<int>ds;
-        int freq[nums.size()+1];
-        for(int i=0;i<=nums.size();i++) freq[i]=0;
+       // vector<int>ds;
+        //int freq[nums.size()+1];
+        //for(int i=0;i<=nums.size();i++) freq[i]=0;
 
-        f(ans,ds,freq,nums);
+        //f(ans,ds,freq,nums);
+
+        f(0,ans,nums);
 
         return ans;
     }
