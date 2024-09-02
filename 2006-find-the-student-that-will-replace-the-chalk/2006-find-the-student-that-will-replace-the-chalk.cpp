6bdb8a2d098b5d5fa1570d523e5f0a06b69dfb48
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-09-02 20:44:24 +0530

    Time: 85 ms (88.75%), Space: 83.9 MB (5.65%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
@@ -1,21 +1,47 @@
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
-        int sum=0;
-        for(int i=0;i<chalk.size();i++){
-            sum+=chalk[i];
-            if(sum>k){
-                break;
-            }
-        }
+        // int sum=0;
+        // for(int i=0;i<chalk.size();i++){
+        //     sum+=chalk[i];
+        //     if(sum>k){
+        //         break;
+        //     }
+        // }
 
-        k=k%sum;
-        for(int i=0;i<chalk.size();i++){
-            if(k<chalk[i]){
-                return i;
-            }
-            k-=chalk[i];
+        // k=k%sum;
+        // for(int i=0;i<chalk.size();i++){
+        //     if(k<chalk[i]){
+        //         return i;
+        //     }
+        //     k-=chalk[i];
+        // }
+        // return 0;
+
+        int n=chalk.size();
+        vector<long>prefixsum(n);
+        prefixsum[0]=chalk[0];
+        for(int i=1;i<n;i++){
+            prefixsum[i]=prefixsum[i-1]+chalk[i];
+
         }
-        return 0;
+        long sum=prefixsum[n-1];
+        long remaining=k%sum;
+        return binarysearch(prefixsum,remaining);
+    }
+
+    int binarysearch(vector<long>&arr,long tar){
+        int low=0;
+        int high=arr.size()-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+           if(arr[mid]<=tar){
+            low=mid+1;
+
+           }else{
+            high=mid;
+           }
+        }
+        return high;
     }
 };
