4722d3cd4ec27db11f1a5291887f1c7701f1ac27
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-09-22 23:20:47 +0530

    Time: 204 ms (44.44%), Space: 46.6 MB (11.11%) - LeetHub

---
+++ 3572-count-substrings-that-can-be-rearranged-to-contain-a-string-ii/3572-count-substrings-that-can-be-rearranged-to-contain-a-string-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    long long validSubstringCount(string word1, string word2) {
+        vector<int>hash(26,0);
+        for(char c:word2){
+            hash[c-'a']++;
+        }
+
+        int start=0;
+        int k=word2.size();
+        long long c=0;
+        vector<int>cnt(26,0);
+        for(int i=0;i<word1.size();i++){
+            char curr=word1[i];
+            if(hash[curr-'a']>0){
+                if(cnt[curr-'a']<hash[curr-'a']){
+                    k--;
+                }
+            }
+            cnt[curr-'a']++;
+            while(k==0){
+                c+=word1.size()-i;
+                char pre=word1[start];
+                cnt[pre-'a']--;
+                if(hash[pre-'a']>0 && cnt[pre-'a']<hash[pre-'a']){
+                    k++;
+                }
+                start++;
+            }
+        }
+        return c;
+    }
+};
