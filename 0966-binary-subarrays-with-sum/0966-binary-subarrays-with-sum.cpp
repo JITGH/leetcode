9f719831271136c2ec481be56dd03c7b74ac2fd8
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-09-26 18:10:41 +0530

    Time: 23 ms (57.48%), Space: 31.5 MB (40.29%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,24 +1,26 @@
 class Solution {
     private:
-    int slidwin(vector<int>& nums, int goal){
-        int cursum=0;
-        int cnt=0;
-        int st=0;
-        for(int j=0;j<nums.size();j++){
-            cursum+=nums[j];
-            while(st<=j && cursum>goal){
-                cursum-=nums[st++];
-               // st++;
-            }
-            cnt+=(j-st+1);
+    // two pass of sliding window
+    // int slidwin(vector<int>& nums, int goal){
+    //     int cursum=0;
+    //     int cnt=0;
+    //     int st=0;
+    //     for(int j=0;j<nums.size();j++){
+    //         cursum+=nums[j];
+    //         while(st<=j && cursum>goal){
+    //             cursum-=nums[st++];
+    //            // st++;
+    //         }
+    //         cnt+=(j-st+1);
 
-        }
-        return cnt;
-    }
+    //     }
+    //     return cnt;
+  //  }
 
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-    //    int n=nums.size();
+        //map and track with current sum
+        int n=nums.size();
     //    int cnt=0;
     //    int cursum=0;
     //    unordered_map<int,int>mpp;
@@ -34,7 +36,28 @@ public:
     //    }
     // return cnt;
 
-    return slidwin(nums,goal)-slidwin(nums,goal-1);
-  
+    //return slidwin(nums,goal)-slidwin(nums,goal-1);
+    int cursum=0;
+    int st=0;
+    int cnt=0;
+    int prefixzeros=0;
+    for(int end=0;end<n;end++){
+        cursum+=nums[end];
+
+        while(st<end && (nums[st]==0||cursum>goal)){
+            if(nums[st]==1){
+                prefixzeros=0;
+            }else{
+                prefixzeros+=1;
+            }
+            cursum-=nums[st];
+            st++;
+        }
+        if(cursum==goal){
+            cnt+=1+prefixzeros;
+        }
+
+    }
+  return cnt;
     }
 };
