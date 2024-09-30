76bf1f9624490d6afe3b54e3a461a60065452e14
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-09-30 12:27:12 +0530

    Time: 23 ms (86.72%), Space: 26.1 MB (10.99%) - LeetHub

--- 1497-design-a-stack-with-increment-operation/1497-design-a-stack-with-increment-operation.cpp
+++ 1497-design-a-stack-with-increment-operation/1497-design-a-stack-with-increment-operation.cpp
@@ -1,32 +1,46 @@
 class CustomStack {
     private:
-    vector<int>v;
-int top_ind;
+//     vector<int>v;
+// int top_ind;
+list<int>stack;
+int maxSize;
 public:
 
     CustomStack(int maxSize) {
-        v.resize(maxSize);
-        top_ind=-1;
+        // v.resize(maxSize);
+        // top_ind=-1;
+        this->maxSize=maxSize;
     }
     
     void push(int x) {
-        if(top_ind<(int)(v.size())-1){
-           v[++top_ind]=x;
+        // if(top_ind<(int)(v.size())-1){
+        //    v[++top_ind]=x;
+        // }
+        if(stack.size()<maxSize){
+            stack.push_back(x);
         }
     }
     
     int pop() {
-        if(top_ind>-1){
-            return v[top_ind--];
-        }else{
-            return -1;
-        }
+        // if(top_ind>-1){
+        //     return v[top_ind--];
+        // }else{
+        //     return -1;
+        // }
+        if(stack.empty()) return -1;
+        int topelement=stack.back();
+        stack.pop_back();
+        return topelement;
     }
     
     void increment(int k, int val) {
-        int limit=min(k,top_ind+1);
-        for(int i=0;i<limit;i++){
-            v[i]+=val;
+        // int limit=min(k,top_ind+1);
+        // for(int i=0;i<limit;i++){
+        //     v[i]+=val;
+        // }
+        auto it=stack.begin();
+        for(int i=0;i<k && it!=stack.end();i++,it++){
+            *it+=val;
         }
     }
 };
