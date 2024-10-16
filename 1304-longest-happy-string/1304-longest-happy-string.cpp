15e8963f74fcef304da7c88c040c458ff8e041ee
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-10-16 14:30:37 +0530

    Time: 3 ms (43.32%), Space: 7.6 MB (78.9%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.cpp
+++ 1304-longest-happy-string/1304-longest-happy-string.cpp
@@ -1,38 +1,84 @@
+// class Solution {
+// public:
+//     string longestDiverseString(int a, int b, int c) {
+//         priority_queue<pair<int,char>>pq;
+//         if(a>0){
+//             pq.push({a,'a'});
+//         }
+//         if(b>0){
+//             pq.push({b,'b'});
+//         }
+//         if(c>0){
+//             pq.push({c,'c'});
+//         }
+
+//         string ans="";
+//         while(!pq.empty()){
+//             pair<int,char>p=pq.top();
+//             pq.pop();
+//             int count=p.first;
+//             char character=p.second;
+
+//             if(ans.length()>=2 && ans[ans.length()-1]==p.second && ans[ans.length()-2]==p.second){
+//                 if(pq.empty()) break;
+//                 pair<int,char>temp=pq.top();
+//                 pq.pop();
+//                 ans+=temp.second;
+//                 if(temp.first-1>0){
+//                     pq.push({temp.first-1,temp.second});
+//                 }
+//             }else{
+//                 count--;
+//                 ans=ans+character;
+//             }
+//             if(count>0){
+//                 pq.push({count,character});
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     string longestDiverseString(int a, int b, int c) {
-        priority_queue<pair<int,char>>pq;
-        if(a>0){
-            pq.push({a,'a'});
-        }
-        if(b>0){
-            pq.push({b,'b'});
-        }
-        if(c>0){
-            pq.push({c,'c'});
-        }
+        int curra = 0, currb = 0, currc = 0;
+        // Maximum total iterations possible is given by the sum of a, b and c.
+        int totalIterations = a + b + c;
+        string ans = "";
 
-        string ans="";
-        while(!pq.empty()){
-            pair<int,char>p=pq.top();
-            pq.pop();
-            int count=p.first;
-            char character=p.second;
-
-            if(ans.length()>=2 && ans[ans.length()-1]==p.second && ans[ans.length()-2]==p.second){
-                if(pq.empty()) break;
-                pair<int,char>temp=pq.top();
-                pq.pop();
-                ans+=temp.second;
-                if(temp.first-1>0){
-                    pq.push({temp.first-1,temp.second});
-                }
-            }else{
-                count--;
-                ans=ans+character;
-            }
-            if(count>0){
-                pq.push({count,character});
+        for (int i = 0; i < totalIterations; i++) {
+            if ((a >= b and a >= c and curra != 2) or
+                (a > 0 and (currb == 2 or currc == 2))) {
+                // If 'a' is maximum and it's streak is less than 2, or if
+                // streak of 'b' or 'c' is 2, then 'a' will be the next
+                // character.
+                ans += 'a';
+                a--;
+                curra++;
+                currb = 0;
+                currc = 0;
+            } else if ((b >= a and b >= c and currb != 2) or
+                       (b > 0 and (currc == 2 or curra == 2))) {
+                // If 'b' is maximum and it's streak is less than 2, or if
+                // streak of 'a' or 'c' is 2, then 'b' will be the next
+                // character.
+                ans += 'b';
+                b--;
+                currb++;
+                curra = 0;
+                currc = 0;
+            } else if ((c >= a and c >= b and currc != 2) or
+                       (c > 0 and (curra == 2 or currb == 2))) {
+                // If 'c' is maximum and it's streak is less than 2, or if
+                // streak of 'a' or 'b' is 2, then 'c' will be the next
+                // character.
+                ans += 'c';
+                c--;
+                currc++;
+                curra = 0;
+                currb = 0;
             }
         }
         return ans;
