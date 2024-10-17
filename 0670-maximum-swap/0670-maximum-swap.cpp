64601a62903d9b32d4fc5b530919a8bf668f8e7e
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-10-17 10:23:47 +0530

    Time: 0 ms (100%), Space: 7.7 MB (31.77%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     int maximumSwap(int num) {
-        string s=to_string(num);
-        int n=s.size();
-        int maxnum=num;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                swap(s[i],s[j]);
-                maxnum=max(maxnum,stoi(s));
-                swap(s[i],s[j]);
+        string numstr=to_string(num);
+        int n=numstr.size();
+        // int maxnum=num;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         swap(s[i],s[j]);
+        //         maxnum=max(maxnum,stoi(s));
+        //         swap(s[i],s[j]);
+        //     }
+        // }
+        // return maxnum;
+        int swapidx1=-1,swapidx2=-1,maxdigitIdx=-1;
+        for(int i=n-1;i>=0;i--){
+            if(maxdigitIdx==-1||numstr[i]>numstr[maxdigitIdx]){
+                maxdigitIdx=i;
+            }else if(numstr[i]<numstr[maxdigitIdx]){
+                swapidx1=i;
+                swapidx2=maxdigitIdx;
             }
         }
-        return maxnum;
+        if(swapidx1!=-1 && swapidx2!=-1){
+            swap(numstr[swapidx1],numstr[swapidx2]);
+        }
+        return stoi(numstr);
     }
 };
