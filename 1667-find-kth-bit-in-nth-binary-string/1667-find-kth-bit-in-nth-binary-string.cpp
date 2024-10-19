595a61a54c1b330546e862b82fdc4c87365f3ac1
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-10-19 10:13:31 +0530

    Time: 54 ms (23.21%), Space: 38.9 MB (52.05%) - LeetHub

--- 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
@@ -1,3 +1,22 @@
+// class Solution {
+// public:
+//     char findKthBit(int n, int k) {
+//         string sequence="0";
+
+//         for(int i=1;i<n && k>sequence.length();i++){
+//             sequence+='1';
+
+//             string temp=sequence;
+//             for(int j=temp.length()-2;j>=0;--j){
+//                 char invertbit=(temp[j]=='1')?'0':'1';
+//                 sequence+=invertbit;
+//             }
+
+//         }
+//         return sequence[k-1];
+//     }
+// };
+
 class Solution {
 public:
     char findKthBit(int n, int k) {
