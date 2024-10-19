f66e8a65bab5f11df73936b310458ff8d0dc8fe4
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-10-19 10:20:00 +0530

    Time: 0 ms (100%), Space: 7.3 MB (87.54%) - LeetHub

--- 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
@@ -1,3 +1,22 @@
+// // class Solution {
+// // public:
+// //     char findKthBit(int n, int k) {
+// //         string sequence="0";
+
+// //         for(int i=1;i<n && k>sequence.length();i++){
+// //             sequence+='1';
+
+// //             string temp=sequence;
+// //             for(int j=temp.length()-2;j>=0;--j){
+// //                 char invertbit=(temp[j]=='1')?'0':'1';
+// //                 sequence+=invertbit;
+// //             }
+
+// //         }
+// //         return sequence[k-1];
+// //     }
+// // };
+
 // class Solution {
 // public:
 //     char findKthBit(int n, int k) {
@@ -17,21 +36,23 @@
 //     }
 // };
 
-class Solution {
+class Solution{
 public:
-    char findKthBit(int n, int k) {
-        string sequence="0";
+char findKthBit(int n, int k){
+    int invertbitcount=0;
+    int len=(1<<n)-1;
 
-        for(int i=1;i<n && k>sequence.length();i++){
-            sequence+='1';
-
-            string temp=sequence;
-            for(int j=temp.length()-2;j>=0;--j){
-                char invertbit=(temp[j]=='1')?'0':'1';
-                sequence+=invertbit;
-            }
+    while(k>1){
+        if(k==len/2+1){
+            return invertbitcount%2==0?'1':'0';
 
         }
-        return sequence[k-1];
+        if(k>len/2){
+            k=len+1-k;
+            invertbitcount++;
+        }
+        len/=2;
     }
+    return invertbitcount%2==0?'0':'1';
+}
 };
