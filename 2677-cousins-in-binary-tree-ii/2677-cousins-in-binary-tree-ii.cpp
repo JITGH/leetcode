09e48700a67e40b7a8ed54bdfe2feec13f13eae7
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-10-23 10:24:38 +0530

    Time: 116 ms (93.32%), Space: 485 MB (5.57%) - LeetHub

--- 2677-cousins-in-binary-tree-ii/2677-cousins-in-binary-tree-ii.cpp
+++ 2677-cousins-in-binary-tree-ii/2677-cousins-in-binary-tree-ii.cpp
@@ -15,42 +15,74 @@ public:
         if(!root) return root;
         queue<TreeNode*>q;
         q.push(root);
-        vector<int>levelsum;
-        while(!q.empty()){
-            int size=q.size();
-            int lsum=0;
-            for(int i=0;i<size;i++){
-                TreeNode* node=q.front();
-                q.pop();
-                lsum+=node->val;
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
-            }
-            levelsum.push_back(lsum);
-        }
+        // TWO PASS OF BFS
+        // vector<int>levelsum;
+        // while(!q.empty()){
+        //     int size=q.size();
+        //     int lsum=0;
+        //     for(int i=0;i<size;i++){
+        //         TreeNode* node=q.front();
+        //         q.pop();
+        //         lsum+=node->val;
+        //         if(node->left) q.push(node->left);
+        //         if(node->right) q.push(node->right);
+        //     }
+        //     levelsum.push_back(lsum);
+        // }
 
-        q.push(root);
-        int ind=1;
-        root->val=0;
+        // q.push(root);
+        // int ind=1;
+        // root->val=0;
+        // while(!q.empty()){
+        //     int size=q.size();
+        //     for(int i=0;i<size;i++){
+        //         TreeNode* node=q.front();
+        //         q.pop();
+        //         int siblings=(node->left?node->left->val:0)+
+        //         (node->right?node->right->val:0);
+
+        //         if(node->left){
+        //             node->left->val=levelsum[ind]-siblings;
+        //             q.push(node->left);
+        //         }
+        //         if(node->right){
+        //             node->right->val=levelsum[ind]-siblings;
+        //             q.push(node->right);
+        //         }
+        //     }
+        //     ind++;
+        // }
+        // return root;
+
+        //BFS WITH RUNNING SUM
+
+        int previoussum=root->val;
+        
         while(!q.empty()){
             int size=q.size();
+            queue<TreeNode*>current;
+            int currsum=0;
             for(int i=0;i<size;i++){
                 TreeNode* node=q.front();
                 q.pop();
-                int siblings=(node->left?node->left->val:0)+
+                node->val=previoussum - node->val;
+
+                int siblingsum=(node->left?node->left->val:0)+
                 (node->right?node->right->val:0);
 
-                if(node->left){
-                    node->left->val=levelsum[ind]-siblings;
+                if(node->left!=NULL){
+                    currsum+=node->left->val;
+                    node->left->val=siblingsum;
                     q.push(node->left);
                 }
-                if(node->right){
-                    node->right->val=levelsum[ind]-siblings;
+                if(node->right!=NULL){
+                    currsum+=node->right->val;
+                    node->right->val=siblingsum;
                     q.push(node->right);
                 }
             }
-            ind++;
+            previoussum=currsum;
         }
-        return root;
+return root;
     }
 };
