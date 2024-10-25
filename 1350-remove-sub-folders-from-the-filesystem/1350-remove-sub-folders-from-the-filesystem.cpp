0c1934c7c7e0b14657dc57628525ab05e2549e3c
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-10-25 10:35:11 +0530

    Time: 46 ms (95.88%), Space: 47.8 MB (75.82%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
     vector<string> removeSubfolders(vector<string>& folder) {
-        unordered_set<string>st(folder.begin(),folder.end());
-        vector<string>ans;
-        for(string& f:folder){
-            bool isSubFolder=false;
-            string prefix=f;
+        // unordered_set<string>st(folder.begin(),folder.end());
+        // vector<string>ans;
+        // for(string& f:folder){
+        //     bool isSubFolder=false;
+        //     string prefix=f;
 
-            while(!prefix.empty()){
-                size_t pos=prefix.find_last_of('/');
-                if(pos==string::npos) break;
+        //     while(!prefix.empty()){
+        //         size_t pos=prefix.find_last_of('/');
+        //         if(pos==string::npos) break;
 
-                prefix=prefix.substr(0,pos);
+        //         prefix=prefix.substr(0,pos);
 
-                if(st.count(prefix)){
-                    isSubFolder=true;
-                    break;
-                }
-            }
-            if(!isSubFolder){
-                ans.push_back(f);
+        //         if(st.count(prefix)){
+        //             isSubFolder=true;
+        //             break;
+        //         }
+        //     }
+        //     if(!isSubFolder){
+        //         ans.push_back(f);
+        //     }
+        // }
+        // return ans;
+
+        sort(folder.begin(),folder.end());
+
+        vector<string>result;
+        result.push_back(folder[0]);
+
+        for(int i=1;i<folder.size();i++){
+            string lastfolder=result.back();
+            lastfolder+='/';
+
+            if(folder[i].compare(0,lastfolder.size(),lastfolder)!=0){
+                result.push_back(folder[i]);
             }
         }
-        return ans;
+        return result;
     }
 };
