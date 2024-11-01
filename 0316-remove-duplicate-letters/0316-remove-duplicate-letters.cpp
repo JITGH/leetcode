82e258a9af218ba710d8b20b4ccfd8528a1adfa0
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-11-01 15:38:47 +0530

    Time: 3 ms (28.4%), Space: 8.6 MB (70.41%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -1,31 +1,58 @@
+// class Solution {
+// public:
+//     string removeDuplicateLetters(string s) {
+//         stack<char>st;
+//         unordered_set<char>seen;
+//         unordered_map<char,int>last_occ;
+
+//         for(int i=0;i<s.size();i++){
+//             last_occ[s[i]]=i;
+//         }
+
+//         for(int i=0;i<s.size();i++){
+//             char c=s[i];
+//             if(seen.find(c)==seen.end()){
+//                 while(!st.empty()&&c<st.top()&&i<last_occ[st.top()]){
+//                     seen.erase(st.top());
+//                     st.pop();
+//                 }
+//                 seen.insert(c);
+//                 st.push(c);
+//             }
+//         }
+
+//         string result="";
+//         while(!st.empty()){
+//             result=st.top()+result;
+//             st.pop();
+//         }
+//         return result;
+//     }
+// };
+
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
+        int n = s.size();
+        string ans = "";
         stack<char>st;
-        unordered_set<char>seen;
-        unordered_map<char,int>last_occ;
-
-        for(int i=0;i<s.size();i++){
-            last_occ[s[i]]=i;
-        }
-
-        for(int i=0;i<s.size();i++){
-            char c=s[i];
-            if(seen.find(c)==seen.end()){
-                while(!st.empty()&&c<st.top()&&i<last_occ[st.top()]){
-                    seen.erase(st.top());
-                    st.pop();
-                }
-                seen.insert(c);
-                st.push(c);
+        vector<int>mp(26);
+        vector<int>instack(26);
+        for(int i=0;i<n;i++) mp[s[i]-'a']++;
+        for(int i=0;i<n;i++){
+            while(!st.empty() && st.top()>=s[i] && mp[st.top()-'a']>1 && instack[s[i]-'a']==0){
+                mp[st.top()-'a']--;
+                instack[st.top()-'a'] = 0;
+                st.pop();
             }
+            if(instack[s[i]-'a']==0) instack[s[i]-'a']=1, st.push(s[i]);
+            else mp[s[i]-'a']--;
         }
-
-        string result="";
         while(!st.empty()){
-            result=st.top()+result;
+            ans+=st.top();
             st.pop();
         }
-        return result;
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
