e0481c13ddb89ae9abbaac2270bea04d66b00ab2
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-11-06 09:41:12 +0530

    Time: 0 ms (100%), Space: 31.7 MB (42.29%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -18,30 +18,57 @@ public:
         // }
         // return true;
 
-        int numofset=__builtin_popcount(values[0]);
-        int maxofseg=values[0];
-        int minofseg=values[0];
+        // int numofset=__builtin_popcount(values[0]);
+        // int maxofseg=values[0];
+        // int minofseg=values[0];
 
 
-        int maxprevseg=INT_MIN;
-        for(int i=1;i<n;i++){
-            if(__builtin_popcount(values[i])==numofset){
-                maxofseg=max(maxofseg,values[i]);
-                minofseg=min(minofseg,values[i]);
+        // int maxprevseg=INT_MIN;
+        // for(int i=1;i<n;i++){
+        //     if(__builtin_popcount(values[i])==numofset){
+        //         maxofseg=max(maxofseg,values[i]);
+        //         minofseg=min(minofseg,values[i]);
+        //     }else{
+        //         if(minofseg<maxprevseg){
+        //             return false;
+        //         }
+        //         maxprevseg=maxofseg;
+
+        //         maxofseg=values[i];
+        //         minofseg=values[i];
+        //         numofset=__builtin_popcount(values[i]);
+        //     }
+        // }
+        // if(minofseg<maxprevseg){
+        //     return false;
+        // }
+        // return true;
+
+
+        for(int i=0;i<n-1;i++){
+            if(values[i]<=values[i+1]){
+                continue;
             }else{
-                if(minofseg<maxprevseg){
+                if(__builtin_popcount(values[i])==__builtin_popcount(values[i+1])){
+                    swap(values[i],values[i+1]);
+                }else{
                     return false;
                 }
-                maxprevseg=maxofseg;
-
-                maxofseg=values[i];
-                minofseg=values[i];
-                numofset=__builtin_popcount(values[i]);
             }
         }
-        if(minofseg<maxprevseg){
-            return false;
+
+        for(int i=n-1;i>=1;i--){
+            if(values[i]>=values[i-1]){
+                continue;
+            }else{
+                if(__builtin_popcount(values[i])==__builtin_popcount(values[i-1])){
+                    swap(values[i],values[i-1]);
+                }else{
+                    return false;
+                }
+            }
         }
+
         return true;
     }
 };
