ab4c9f7014142325d5a344c7f65867a1fcaa6ec4
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-11-08 09:35:38 +0530

    Time: 4 ms (57.14%), Space: 95.4 MB (82.32%) - LeetHub

--- 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.cpp
+++ 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.cpp
@@ -2,45 +2,56 @@ class Solution {
 public:
     vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
         int n=nums.size();
-        vector<int>ans(n);
-        vector<int>pre(n);
-        pre[0]=nums[0];
+    //     vector<int>ans(n);
+    //     vector<int>pre(n);
+    //     pre[0]=nums[0];
         int target=(1<<maximumBit)-1;
-    //   // cout<<target<<" ";
-    //     // for(int i=0;i<n;i++){
-    //     //     xor1^=nums[i];
-    //     // }
-    //     int j=n-1;
+    // //   // cout<<target<<" ";
+    // //     // for(int i=0;i<n;i++){
+    // //     //     xor1^=nums[i];
+    // //     // }
+    // //     int j=n-1;
 
-    //     while(j>=0){
-    //          int xor1=0;
-    //         for(int i=0;i<=j;i++){
-    //             xor1^=nums[i];
-    //             //cout<<xor1<<" ";
-    //         }
-    //         for(int a=0;a<target;a++){
-    //            // xor1^=a;
-    //             if((xor1^a)==(target-1)){
-    //                 ans.push_back(a);
-    //                // xor1^=k;
-    //                 break;
-    //            }
+    // //     while(j>=0){
+    // //          int xor1=0;
+    // //         for(int i=0;i<=j;i++){
+    // //             xor1^=nums[i];
+    // //             //cout<<xor1<<" ";
+    // //         }
+    // //         for(int a=0;a<target;a++){
+    // //            // xor1^=a;
+    // //             if((xor1^a)==(target-1)){
+    // //                 ans.push_back(a);
+    // //                // xor1^=k;
+    // //                 break;
+    // //            }
             
-    //         }
-    //       //  xor1^=nums[j];
-    //         j--;
+    // //         }
+    // //       //  xor1^=nums[j];
+    // //         j--;
+    // //     }
+
+    // //     return ans;
+
+    //     for(int i=1;i<n;i++){
+    //         pre[i]=pre[i-1]^nums[i];
+    //     }
+    //     for(int i=0;i<n;i++){
+    //         int curr=pre[n-i-1];
+    //         ans[i]=curr^target;
     //     }
 
     //     return ans;
 
-        for(int i=1;i<n;i++){
-            pre[i]=pre[i-1]^nums[i];
-        }
-        for(int i=0;i<n;i++){
-            int curr=pre[n-i-1];
-            ans[i]=curr^target;
-        }
-
-        return ans;
+    int xor1=0;
+    for(int i=0;i<n;i++){
+        xor1^=nums[i];
+    }
+    vector<int>ans(n);
+    for(int i=0;i<n;i++){
+        ans[i]=xor1^target;
+        xor1=xor1^nums[n-i-1];
+    }
+return ans;
     }
 };
