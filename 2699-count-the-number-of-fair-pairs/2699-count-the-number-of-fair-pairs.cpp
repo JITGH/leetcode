0617356aa962b817abd642367048bfea67fa74d8
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-11-13 12:42:27 +0530

    Time: 23 ms (98.81%), Space: 60.2 MB (94.04%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
@@ -18,28 +18,48 @@ public:
         // return cnt;
 
         sort(nums.begin(),nums.end());
-        long long ans=0;
-        for(int i=0;i<n;i++){
-            int low=lower_bound(nums,i+1,nums.size()-1,lower-nums[i]);
+        // long long ans=0;
+        // for(int i=0;i<n;i++){
+        //     int low=lower_bound(nums,i+1,nums.size()-1,lower-nums[i]);
 
-            int high=lower_bound(nums,i+1,nums.size()-1,upper-nums[i]+1);
+        //     int high=lower_bound(nums,i+1,nums.size()-1,upper-nums[i]+1);
 
-            ans+=1LL*(high-low);
-        }
-        return ans;
+        //     ans+=1LL*(high-low);
+        // }
+        // return ans;
+
+        return lower_bound(nums,upper+1)-lower_bound(nums,lower);
     }
 
 
-    long long lower_bound(vector<int>&nums,int low,int high,int element){
-        while(low<=high){
-            int mid=low+(high-low)/2;
+    // long long lower_bound(vector<int>&nums,int low,int high,int element){
+    //     while(low<=high){
+    //         int mid=low+(high-low)/2;
 
-            if(nums[mid]>=element){
-                high=mid-1;
+    //         if(nums[mid]>=element){
+    //             high=mid-1;
+    //         }else{
+    //             low=mid+1;
+    //         }
+    //     }
+    //     return low;
+    // }
+
+     long long lower_bound(vector<int>&nums,int element){
+        int left=0;
+        int right=nums.size()-1;
+        long long result=0;
+        while(left<=right){
+            int sum=nums[left]+nums[right];
+
+            if(sum<element){
+                result+=(right-left);
+                left++;
             }else{
-                low=mid+1;
+                right--;
             }
         }
-        return low;
-    }
+        return result;
+
+     }
 };
