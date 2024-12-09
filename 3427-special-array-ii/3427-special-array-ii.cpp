8eeae96414f97ab4f72b3f3e7564838778b65862
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-12-09 09:33:14 +0530

    Time: 43 ms (35.94%), Space: 136.4 MB (24.44%) - LeetHub

--- 3427-special-array-ii/3427-special-array-ii.cpp
+++ 3427-special-array-ii/3427-special-array-ii.cpp
@@ -4,44 +4,60 @@ public:
         int n=a.size();
         int m=queries.size();
         int t=queries[0].size();
-        vector<bool>ans(m);
-        vector<int>vio;
+        vector<bool>ans(m,false);
+        vector<int>pre(n,0);
+        pre[0]=0;
         for(int i=1;i<n;i++){
             if(a[i]%2==a[i-1]%2){
-                vio.push_back(i);
+                pre[i]=pre[i-1]+1;
+            }else{
+                pre[i]=pre[i-1];
             }
         }
+
         for(int i=0;i<m;i++){
-            vector<int>querie=queries[i];
-            int from=querie[0];
-            int to=querie[1];
+            vector<int>query=queries[i];
+            int st=query[0];
+            int en=query[1];
+            ans[i]=pre[en]-pre[st]==0;
+        }
+        // vector<int>vio;
+        // for(int i=1;i<n;i++){
+        //     if(a[i]%2==a[i-1]%2){
+        //         vio.push_back(i);
+        //     }
+        // }
+        // for(int i=0;i<m;i++){
+        //     vector<int>querie=queries[i];
+        //     int from=querie[0];
+        //     int to=querie[1];
             
-            bool found=binarysearch(from+1,to,vio);
-            if(found){
-                ans[i]=false;
-            }else{
-              ans[i]=true;
-            }
-        }
-        return ans;
+        //     bool found=binarysearch(from+1,to,vio);
+        //     if(found){
+        //         ans[i]=false;
+        //     }else{
+        //       ans[i]=true;
+        //     }
+        // }
+         return ans;
     }
 
-    private:
-    bool binarysearch(int st,int en,vector<int>&vio){
-        int left=0;
-        int right=vio.size()-1;
-        while(left<=right){
-            int mid=left+(right-left)/2;
-            int vin=vio[mid];
+    // private:
+    // bool binarysearch(int st,int en,vector<int>&vio){
+    //     int left=0;
+    //     int right=vio.size()-1;
+    //     while(left<=right){
+    //         int mid=left+(right-left)/2;
+    //         int vin=vio[mid];
 
-            if(vin<st){
-                left=mid+1;
-            }else if(vin>en){
-                right=mid-1;
-            }else{
-                return true;
-            }
-        }
-        return false;
-    }
+    //         if(vin<st){
+    //             left=mid+1;
+    //         }else if(vin>en){
+    //             right=mid-1;
+    //         }else{
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
 };
