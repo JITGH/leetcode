2535d8be6ea7adf65b4f545e1ac096b30b102c7f
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-12-13 10:48:22 +0530

    Time: 60 ms (86.28%), Space: 98.8 MB (90.85%) - LeetHub

--- 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.cpp
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
     long long findScore(vector<int>& nums) {
-        long long ans=0;
-        vector<bool>marked(nums.size());
-        auto cmp=[](pair<int,int>&pair1,pair<int,int>&pair2){
-            if(pair1.first!=pair2.first) return pair1.first>pair2.first;
-            return pair1.second>pair2.second;
-        };
-
-        priority_queue<pair<int,int>,vector<pair<int,int>>,decltype(cmp)>pq(cmp);
-        for(int i=0;i<nums.size();i++){
-            pq.push(make_pair(nums[i],i));
+        int n = nums.size();
+        vector<pair<int, int>> numIndex(n);
+        for (int i = 0; i < n; i++) {
+            numIndex[i] = {nums[i], i};
         }
 
-        while(!pq.empty()){
-            pair<int,int>it=pq.top();
-            pq.pop();
-            int num=it.first;
-            int ind=it.second;
+        sort(numIndex.begin(), numIndex.end());
 
-            if(!marked[ind]){
-                ans+=num;
-                marked[ind]=true;
-            
-            if(ind-1>=0){
-                marked[ind-1]=true;
-            }
-            if(ind+1<nums.size()){
-                marked[ind+1]=true;
+        long long score = 0;
+        vector<bool> marked(n, false);
+        for (int i = 0; i < n; i++) {
+            if (!marked[numIndex[i].second]) {
+                score += numIndex[i].first;
+                marked[max(numIndex[i].second - 1, 0)] = true;
+                marked[numIndex[i].second] = true;
+                marked[min(numIndex[i].second + 1, n - 1)] = true;
             }
         }
-        }
-        return ans;
+
+        return score;
     }
 };
