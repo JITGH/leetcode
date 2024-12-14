a431f6cfd0448edac1011fc9473aa8446fd1ee79
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-12-14 12:50:28 +0530

    Time: 91 ms (83.53%), Space: 111.7 MB (93.28%) - LeetHub

--- 2868-continuous-subarrays/2868-continuous-subarrays.cpp
+++ 2868-continuous-subarrays/2868-continuous-subarrays.cpp
@@ -5,7 +5,8 @@ public:
        int left=0;
        int right=0;
        int n=nums.size();
-       long long count=0;
+       long long cnt=0,windowLen=0;
+       int curMin,curMax;
     
     //    while(right<n){
     //     freq[nums[right]]++;
@@ -20,28 +21,57 @@ public:
     //     count+=right-left+1;
     //     right++;
     //    }
-    priority_queue<int,vector<int>,function<bool(int,int)>>minheap(
-        [&nums](int a,int b) {return nums[a]>nums[b]; });
-    priority_queue<int,vector<int>,function<bool(int,int)>>maxheap(
-        [&nums](int a,int b) {return nums[a]<nums[b];});
+    // priority_queue<int,vector<int>,function<bool(int,int)>>minheap(
+    //     [&nums](int a,int b) {return nums[a]>nums[b]; });
+    // priority_queue<int,vector<int>,function<bool(int,int)>>maxheap(
+    //     [&nums](int a,int b) {return nums[a]<nums[b];});
 
-        while(right<n){
-            minheap.push(right);
-            maxheap.push(right);
+    //     while(right<n){
+    //         minheap.push(right);
+    //         maxheap.push(right);
 
-            while(left<right && nums[maxheap.top()]-nums[minheap.top()]>2){
-                left++;
+    //         while(left<right && nums[maxheap.top()]-nums[minheap.top()]>2){
+    //             left++;
             
-            while(!maxheap.empty() && maxheap.top()<left){
-                maxheap.pop();
-            }
-             while(!minheap.empty() && minheap.top()<left){
-                minheap.pop();
+    //         while(!maxheap.empty() && maxheap.top()<left){
+    //             maxheap.pop();
+    //         }
+    //          while(!minheap.empty() && minheap.top()<left){
+    //             minheap.pop();
+    //         }
+    //     }
+    //         count+=right-left+1;
+    //         right++;
+    //     }
+
+        curMin=curMax=nums[right];
+        for(right=0;right<n;right++){
+            curMin=min(curMin,nums[right]);
+            curMax=max(curMax,nums[right]);
+
+            if(curMax-curMin>2){
+                windowLen=right-left;
+                cnt+=(windowLen*(windowLen+1)/2);
+
+
+                left=right;
+                curMin=curMax=nums[right];
+
+                while(left>0 && abs(nums[right]-nums[left-1])<=2){
+                    left--;
+                    curMin=min(curMin,nums[left]);
+                    curMax=max(curMax,nums[left]);
+
+                }
+                if(left<right){
+                    windowLen=right-left;
+                   cnt-=(windowLen*(windowLen+1)/2);
+                }
             }
         }
-            count+=right-left+1;
-            right++;
-        }
-       return count;
+        windowLen=right-left;
+        cnt+=(windowLen*(windowLen+1)/2);
+
+       return cnt;
     }
 };
