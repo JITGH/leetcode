10eed57e4a9cb88fb78bd5e1121945aa407bf40b
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-12-14 12:20:26 +0530

    Time: 279 ms (9.28%), Space: 122 MB (45.94%) - LeetHub

--- 2868-continuous-subarrays/2868-continuous-subarrays.cpp
+++ 2868-continuous-subarrays/2868-continuous-subarrays.cpp
@@ -7,19 +7,41 @@ public:
        int n=nums.size();
        long long count=0;
     
-       while(right<n){
-        freq[nums[right]]++;
+    //    while(right<n){
+    //     freq[nums[right]]++;
 
-        while(freq.rbegin()->first-freq.begin()->first>2){
-            freq[nums[left]]--;
-            if(freq[nums[left]]==0){
-                freq.erase(nums[left]);
+    //     while(freq.rbegin()->first-freq.begin()->first>2){
+    //         freq[nums[left]]--;
+    //         if(freq[nums[left]]==0){
+    //             freq.erase(nums[left]);
+    //         }
+    //         left++;
+    //     }
+    //     count+=right-left+1;
+    //     right++;
+    //    }
+    priority_queue<int,vector<int>,function<bool(int,int)>>minheap(
+        [&nums](int a,int b) {return nums[a]>nums[b]; });
+    priority_queue<int,vector<int>,function<bool(int,int)>>maxheap(
+        [&nums](int a,int b) {return nums[a]<nums[b];});
+
+        while(right<n){
+            minheap.push(right);
+            maxheap.push(right);
+
+            while(left<right && nums[maxheap.top()]-nums[minheap.top()]>2){
+                left++;
+            
+            while(!maxheap.empty() && maxheap.top()<left){
+                maxheap.pop();
             }
-            left++;
+             while(!minheap.empty() && minheap.top()<left){
+                minheap.pop();
+            }
         }
-        count+=right-left+1;
-        right++;
-       }
+            count+=right-left+1;
+            right++;
+        }
        return count;
     }
 };
