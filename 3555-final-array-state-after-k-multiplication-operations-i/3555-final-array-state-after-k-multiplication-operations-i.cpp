bcc818df6f51dc191cbf2fc7869e768e9e58a40a
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-12-16 10:56:59 +0530

    Time: 1 ms (40.45%), Space: 27.7 MB (29.75%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
@@ -2,15 +2,35 @@ class Solution {
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
         int n=nums.size();
-        for(int i=0;i<k;i++){
-            int mini=0;
-            for(int j=0;j<n;j++){
-                if(nums[j]<nums[mini]){
-                    mini=j;
-                }
-            }
-            nums[mini]*=multiplier;
+        // for(int i=0;i<k;i++){
+        //     int mini=0;
+        //     for(int j=0;j<n;j++){
+        //         if(nums[j]<nums[mini]){
+        //             mini=j;
+        //         }
+        //     }
+        //     nums[mini]*=multiplier;
     
+        // }
+        // return nums;
+
+
+
+        vector<pair<int,int>>heap;
+        for(int i=0;i<n;i++){
+            heap.push_back({nums[i],i});
+        }
+
+        make_heap(heap.begin(),heap.end(),greater<>());
+
+        while(k--){
+            pop_heap(heap.begin(),heap.end(),greater<>());
+            auto[value,i]=heap.back();
+            heap.pop_back();
+
+            nums[i]*=multiplier;
+            heap.push_back({nums[i],i});
+            push_heap(heap.begin(),heap.end(),greater<>());
         }
         return nums;
     }
