c2786cda318286042e4c0719ffc1bdc09240b839
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-12-20 11:34:30 +0530

    Time: 0 ms (100%), Space: 77.2 MB (65.15%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
@@ -11,36 +11,53 @@
  * };
  */
 class Solution {
+    private:
+        void dfs(TreeNode* leftChild,TreeNode* rightChild,int level){
+            if(leftChild==nullptr &&rightChild==nullptr) return;
+
+            if(level%2==0){
+                int temp=leftChild->val;
+                leftChild->val=rightChild->val;
+                rightChild->val=temp;
+            }
+            dfs(leftChild->left,rightChild->right,level+1);
+            dfs(leftChild->right,rightChild->left,level+1);
+        }
 public:
     TreeNode* reverseOddLevels(TreeNode* root) {
-        if (root == nullptr)
-            return nullptr;
-        //  if(root->left==nullptr && root->right==nullptr) return root;
-        int level = 0;
-        queue<TreeNode*> q;
-        q.push(root);
-        while (!q.empty()) {
-            auto size = q.size();
-            vector<TreeNode*> nodes;
-            while (size--) {
-                TreeNode* currentNode = q.front();
-                q.pop();
-                nodes.push_back(currentNode);
+        // if (root == nullptr)
+        //     return nullptr;
 
-                if(currentNode->left) q.push(currentNode->left);
-                if(currentNode->right)  q.push(currentNode->right);
-            }
-            if (level % 2 ==1) {
-                int left = 0;
-                int right = nodes.size() - 1;
-                while (left < right) {
-                    swap(nodes[left]->val, nodes[right]->val);
-                    left++;
-                    right--;
-                }
-            }
-            level++;
-        }
+            //BFS
+        //  if(root->left==nullptr && root->right==nullptr) return root;
+        // int level = 0;
+        // queue<TreeNode*> q;
+        // q.push(root);
+        // while (!q.empty()) {
+        //     auto size = q.size();
+        //     vector<TreeNode*> nodes;
+        //     while (size--) {
+        //         TreeNode* currentNode = q.front();
+        //         q.pop();
+        //         nodes.push_back(currentNode);
+
+        //         if(currentNode->left) q.push(currentNode->left);
+        //         if(currentNode->right)  q.push(currentNode->right);
+        //     }
+        //     if (level % 2 ==1) {
+        //         int left = 0;
+        //         int right = nodes.size() - 1;
+        //         while (left < right) {
+        //             swap(nodes[left]->val, nodes[right]->val);
+        //             left++;
+        //             right--;
+        //         }
+        //     }
+        //     level++;
+        // }
+        // return root;
+
+        dfs(root->left,root->right,0);
         return root;
     }
 };
