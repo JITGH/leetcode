649b58c8f74652f868e5a6499064d2af29334148
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-12-25 11:53:31 +0530

    Time: 0 ms (100%), Space: 10.8 MB (88.18%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -4,26 +4,40 @@ public:
         if(numRows==1||numRows>=s.size()){
             return s;
         }
-        int ind=0;
-        int d=1;
-        vector<vector<char>>rows(numRows);
-        for(char c:s){
-            rows[ind].push_back(c);
-            if(ind==0){
-                d=1;
-            }if(ind==numRows-1){
-                d=-1;
-            }
-            ind+=d;
-        }
+        // int ind=0;
+        // int d=1;
+        // vector<vector<char>>rows(numRows);
+        // for(char c:s){
+        //     rows[ind].push_back(c);
+        //     if(ind==0){
+        //         d=1;
+        //     }if(ind==numRows-1){
+        //         d=-1;
+        //     }
+        //     ind+=d;
+        // }
 
-        string result;
-        for(const auto& row:rows){
-            for(char c:row){
-                result+=c;
+        // string result;
+        // for(const auto& row:rows){
+        //     for(char c:row){
+        //         result+=c;
+        //     }
+        // }
+
+        // return result;
+
+
+        string result="";
+        int cycleLen=2*numRows-2;
+        for(int i=0;i<numRows;i++){
+            for(int j=0;j+i<s.size();j+=cycleLen){
+                result+=s[j+i];
+
+                if(i!=0 && i!=numRows-1 && j+cycleLen-i<s.size()){
+                    result+=s[j+cycleLen-i];
+                }
             }
         }
-
         return result;
     }
 };
