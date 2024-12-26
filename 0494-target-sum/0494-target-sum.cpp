3c8dd3744b8482c63ac382889f0a1ca226218e03
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2024-12-26 12:20:31 +0530

    Time: 4 ms (81.21%), Space: 15.2 MB (46.77%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,20 +1,52 @@
 class Solution {
+    const int MOD=1e9+7;
     private:
-    int f(int ind,int target,vector<int>& nums,vector<vector<int>>&dp){
-        if(ind==0){
-            if(target==0 && nums[0]==0) return 2;
-            if(target==0||nums[0]==target) return 1;
+    // int f(int ind,int target,vector<int>& nums,vector<vector<int>>&dp){
+    //     if(ind==0){
+    //         if(target==0 && nums[0]==0) return 2;
+    //         if(target==0||nums[0]==target) return 1;
 
-            return 0;
-        }
-        if(dp[ind][target]!=-1) return dp[ind][target];
-        int nottake=f(ind-1,target,nums,dp);
+    //         return 0;
+    //     }
+    //     if(dp[ind][target]!=-1) return dp[ind][target];
+    //     int nottake=f(ind-1,target,nums,dp);
 
-        int take=0;
-        if(nums[ind]<=target)
-        take=f(ind-1,target-nums[ind],nums,dp);
+    //     int take=0;
+    //     if(nums[ind]<=target)
+    //     take=f(ind-1,target-nums[ind],nums,dp);
 
-        return dp[ind][target]=(take+nottake);
+    //     return dp[ind][target]=(take+nottake);
+    // }
+
+    int f(vector<int>& nums, int t){
+          int n=nums.size();
+        vector<vector<int>>dp(n,vector<int>(t+1,0));
+       
+         if(nums[0]==0) dp[0][0]=2;
+         else dp[0][0]=1;
+
+         if(nums[0]!=0 && nums[0]<=t) dp[0][nums[0]]=1;
+
+         for(int ind=1;ind<n;ind++){
+            for(int tar=0;tar<=t;tar++){
+                int nottake=dp[ind-1][tar];
+
+                int take=0;
+                if(nums[ind]<=tar)
+                take=dp[ind-1][tar-nums[ind]];
+
+
+                dp[ind][tar]=(take+nottake)%MOD;
+            }
+         }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<=t;j++){
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<"\n";
+        //  }
+         return dp[n-1][t];
+
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
@@ -24,11 +56,11 @@ public:
             totalSum+=nums[i];
         }
 
-        if(totalSum-target<0) return 0;
-        if((totalSum-target)%2==1) return 0;
+        if(totalSum-target<0 ||(totalSum-target)%2!=0) return 0;
+        // if((totalSum-target)%2==1) return 0;
 
-        int secondOp=(totalSum-target)/2;
-        vector<vector<int>>dp(n,vector<int>(secondOp+1,-1));
-        return f(n-1,secondOp,nums,dp);
+      //  int secondOp=;
+       // vector<vector<int>>dp(n,vector<int>(secondOp+1,-1));
+        return f(nums,(totalSum-target)/2);
     }
 };
