4f190b99cfa101cf015a27ef37bd564e4bed2bbf
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2025-01-04 23:56:49 +0530

    Time: 3 ms (29.67%), Space: 15.6 MB (57.21%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,12 +1,30 @@
 class Solution {
 public:
     int minMoves2(vector<int>& nums) {
-        int n = nums.size(), steps = 0;
-        sort(nums.begin(), nums.end());
-        int median = nums[n/2]; // Finding median
-        for(int i=0; i<n; i++){
-            steps += abs(nums[i] - median); //Adding absolute difference
+        // int n = nums.size(), steps = 0;
+        // sort(nums.begin(), nums.end());
+        // int median = nums[n/2]; // Finding median
+        // for(int i=0; i<n; i++){
+        //     steps += abs(nums[i] - median); //Adding absolute difference
+        // }
+        // return steps;
+
+         sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int moves=0;
+        int med;
+        if(n%2!=0){
+          med=(n-1)/2;
+          for(int i=0;i<n;i++){
+            moves+=abs(nums[med]-nums[i]);
+          }
         }
-        return steps;
+        else{
+            med=n/2;
+            for(int i=0;i<n;i++){
+                moves+=abs(nums[med]-nums[i]);
+            }
+        }
+        return moves;
     }
 };
