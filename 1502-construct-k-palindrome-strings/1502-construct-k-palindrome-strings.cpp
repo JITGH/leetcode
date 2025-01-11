1856ea5a7f552404fc1a07bda26e27d631e2a015
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2025-01-11 10:40:46 +0530

    Time: 8 ms (47.9%), Space: 15 MB (51.75%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.cpp
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.cpp
@@ -3,13 +3,15 @@ public:
     bool canConstruct(string s, int k) {
         if(s.size()<k) return false;
         if(s.size()==k) return true;
-        unordered_map<char,int>mpp;
+       // unordered_map<char,int>mpp;
+       vector<int>hash(26,0);
         for(int i=0;i<s.size();i++){
-            mpp[s[i]]++;
+           // mpp[s[i]]++;
+           hash[s[i]-'a']++;
         }
         int cnt=0;
-        for(auto it:mpp){
-            if(it.second%2==1){
+        for(int i=0;i<26;i++){
+            if(hash[i]%2==1){
                 cnt++;
             }
         }
