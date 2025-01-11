cef54391ccc4d670f4f765ea64d450813f8b4a1b
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2025-01-11 10:42:57 +0530

    Time: 0 ms (100%), Space: 14.9 MB (66.78%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.cpp
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.cpp
@@ -1,20 +1,39 @@
-class Solution {
+
+    //     if(s.size()<k) return false;
+    //     if(s.size()==k) return true;
+    //    // unordered_map<char,int>mpp;
+    //    vector<int>hash(26,0);
+    //     for(int i=0;i<s.size();i++){
+    //        // mpp[s[i]]++;
+    //        hash[s[i]-'a']++;
+    //     }
+    //     int cnt=0;
+    //     for(int i=0;i<26;i++){
+    //         if(hash[i]%2==1){
+    //             cnt++;
+    //         }
+    //     }
+    //     return cnt<=k;
+
+    class Solution {
 public:
     bool canConstruct(string s, int k) {
-        if(s.size()<k) return false;
-        if(s.size()==k) return true;
-       // unordered_map<char,int>mpp;
-       vector<int>hash(26,0);
-        for(int i=0;i<s.size();i++){
-           // mpp[s[i]]++;
-           hash[s[i]-'a']++;
+        // Handle edge cases
+        if (s.length() < k) return false;
+        if (s.length() == k) return true;
+
+        // Initialize oddCount as an integer bitmask
+        int oddCount = 0;
+
+        // Update the bitmask for each character in the string
+        for (char chr : s) {
+            oddCount ^= 1 << (chr - 'a');
         }
-        int cnt=0;
-        for(int i=0;i<26;i++){
-            if(hash[i]%2==1){
-                cnt++;
-            }
-        }
-        return cnt<=k;
+
+        // Count the number of set bits in the bitmask
+        int setBits = __builtin_popcount(oddCount);
+
+        // Return if the number of odd frequencies is less than or equal to k
+        return  setBits<= k;
     }
 };
