6dea31b76f29c767a77bcca78c8bce8af21c7dbf
JIT GHOSH <93810143+JITGH@users.noreply.github.com>
2025-01-22 12:14:57 +0530

    Time: 0 ms (100%), Space: 40.2 MB (58.21%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,55 +1,20 @@
-//Solution 01:
-/**class Solution {
-public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        int n= nums.size(), p=1;
-        vector<int> l(n), r(n);
-        
-        for(int i=0; i<n; i++){
-            p *= nums[i];
-            l[i]=p;
-        }
-        
-        p=1;
-        
-        for(int i=n-1; i>=0; i--){
-            p *= nums[i];
-            r[i]=p;
-        }
-        
-        nums[0]=r[1]; nums[n-1]=l[n-2];
-        
-        for(int i=1; i<n-1; i++){
-            nums[i]= l[i-1]*r[i+1];
-        }
-        
-        return nums;
-    }
-};**/
-
-//Solution 02:
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n= nums.size(), p=1,po=1;
-        vector<int> ans(n);
-        
-        for(int i=0; i<n; i++){
-            // p *= nums[i];
-            // ans[i]=p;
-            ans[i]=p;
+        int p=1;
+        int n=nums.size();
+        vector<int>ans(n,1);
+        for(int i=0;i<n;i++){
+            ans[i]*=p;
             p*=nums[i];
         }
-        
-       // int product=1;
-        for(int i=n-1; i>=0; i--){
-            // ans[i]= ans[i-1]*product;
-            // product *= nums[i];
-            ans[i]*=po;
-            po*=nums[i];
+
+        p=1;
+        for(int i=n-1;i>=0;i--){
+            ans[i]*=p;
+            p*=nums[i];
         }
-       // ans[0]=product;
-        
+
         return ans;
     }
 };
